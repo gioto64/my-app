b4420c8fa4ba39376ec4d5fd393ac91750ec2ee9
gioto64 <87807240+gioto64@users.noreply.github.com>
2022-11-05 23:47:24 +0200

    . (#14)

---
+++ src/components/SpeechToFrontend/micro_components/CustomButton.tsx
@@ -0,0 +1,31 @@
+import { Button, View } from "@adobe/react-spectrum";
+import CodeContainer from "./CodeContainer";
+
+interface CustomButtonInterface {
+    color?: string;
+    text?: string;
+}
+
+const CustomButton = (props: CustomButtonInterface) => {
+  const codeToPrint = `
+import { Button } from "@adobe/react-spectrum";
+const CustomButton = () => {
+  return (
+    <Button variant='cta' UNSAFE_style={{background: ${props.color}}}>
+      ${props.text}
+    </Button>
+  );
+}`;
+
+  return (
+      <View>
+        <Button variant='cta' UNSAFE_style={{background: `${props.color}`}}>
+          {props.text}
+        </Button>
+        <CodeContainer codeToPrint={codeToPrint} />
+      </View>
+
+    )
+}
+
+export default CustomButton;
