9b50a395cd9bb93ca904484c61009997f95caf28
Ioan Popescu <gpopescuioan@gmail.com>
2022-11-06 06:00:22 +0200

    refactor the coding container logic

--- src/components/SpeechToFrontend/store/actions/speech_actions.tsx
+++ src/components/SpeechToFrontend/store/actions/speech_actions.tsx
@@ -3,6 +3,7 @@ import { Component } from "../reducers/speech_reducers";
 export const ADD_COMPONENT = "ADD_COMPONENT";
 export const REMOVE_COMPONENT = "REMOVE_COMPONENT";
 export const ADD_TEXT = "ADD_TEXT";
+export const SHOW_CODE = "SHOW_CODE";
 
 export const addComponent = (component: Component) => ({
   type: ADD_COMPONENT,
@@ -16,5 +17,10 @@ export const removeComponent = (name: string) => ({
 
 export const addText = (text: string) => ({
   type: ADD_TEXT,
+  payload: text,
+});
+
+export const showCode = (text: string) => ({
+  type: SHOW_CODE,
   payload: text,
 });
